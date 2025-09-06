17f703812d4638e8cf89e45ba06a29f15d17582b
misker <miskera22@gmail.com>
2025-09-06 04:15:33 +0300

    week-6-assignment-submission

---
+++ answers.sql
@@ -0,0 +1,39 @@
+-- ### **Question 1 🧑‍💼**  
+-- **Write an SQL query** to get the **firstName**, **lastName**, **email**, and **officeCode** of all employees.  
+-- Use an **INNER JOIN** to combine the **employees** table with the **offices** table using the **officeCode** column.
+USE salesdb;
+SELECT 
+    e.firstName,
+    e.lastName,
+    e.email,
+    e.officeCode
+FROM employees e
+INNER JOIN offices o 
+    ON e.officeCode = o.officeCode;
+
+    -- ### **Question 2 🛍️**  
+-- **Write an SQL query** to get the **productName**, **productVendor**, and **productLine** from the **products** table.  
+-- Use a **LEFT JOIN** to combine the **products** table with the **productlines** table using the **productLine** column.
+USE salesdb;
+SELECT 
+    p.productName,
+    p.productVendor,
+    p.productLine
+FROM products p
+LEFT JOIN productlines pl 
+    ON p.productLine = pl.productLine;
+    
+    -- ### **Question 3 📦**  
+-- **Write an SQL query** to retrieve the **orderDate**, **shippedDate**, **status**, and **customerNumber** for the first 10 orders.  
+-- Use a **RIGHT JOIN** to combine the **customers** table with the **orders** table using the **customerNumber** column.
+
+USE salesdb;
+SELECT 
+    o.orderDate,
+    o.shippedDate,
+    o.status,
+    o.customerNumber
+FROM customers c
+RIGHT JOIN orders o 
+    ON c.customerNumber = o.customerNumber
+LIMIT 10;
